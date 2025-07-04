da9d2ec96a59c4904fa2b3715166557e81a7df4d
Victor Manuel Martinez Cruz <vicmarc30@gmail.com>
2025-07-04 16:13:35 -0600

    fix: update header text in PeliculaPage to include greeting

--- src/app/dashboard/peliculas/[id]/page.tsx
+++ src/app/dashboard/peliculas/[id]/page.tsx
@@ -5,7 +5,7 @@ export default async function PeliculaPage({ params }) {
     <div className="h-[80dvh] text-center flex flex-col items-center justify-center gap-10 px-8">
       <div className="flex flex-col gap-2 absolute top-24 left-1/2 -translate-x-1/2">
         <h1 className="text-xl md:text-2xl font-bold">
-          Crear nueva película en {id}
+          Crear nueva película en {id} hola
         </h1>
       </div>
       {/* Aquí iría el formulario de creación de película */}
