c6431a6d19c0a93010833aad8236425cdc1deb67
Victor Manuel Martinez Cruz <vicmarc30@gmail.com>
2025-07-04 16:20:07 -0600

    fix: refactor PeliculaPage to use async params and display movie ID

--- src/app/dashboard/peliculas/[id]/page.tsx
+++ src/app/dashboard/peliculas/[id]/page.tsx
@@ -1,12 +1,8 @@
-export default function PeliculaPage({ params }: { params: { id: string } }) {
-  return (
-    <div className="h-[80dvh] text-center flex flex-col items-center justify-center gap-10 px-8">
-      <div className="flex flex-col gap-2 absolute top-24 left-1/2 -translate-x-1/2">
-        <h1 className="text-xl md:text-2xl font-bold">
-          Crear nueva película en {params.id}
-        </h1>
-      </div>
-      {/* Aquí iría el formulario de creación de película */}
-    </div>
-  )
+export default async function PeliculaPage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
+  return <div>My Post: {id}</div>
 }
