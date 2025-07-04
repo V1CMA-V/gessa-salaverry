344478eb32621ac19b63ea0cf031819c1f119710
Victor Manuel Martinez Cruz <vicmarc30@gmail.com>
2025-07-04 16:17:18 -0600

    fix: update PeliculaPage to correctly extract and display movie ID from params

--- src/app/dashboard/peliculas/[id]/page.tsx
+++ src/app/dashboard/peliculas/[id]/page.tsx
@@ -1,11 +1,9 @@
-export default async function PeliculaPage({ params }) {
-  const { id } = await params
-
+export default function PeliculaPage({ params }: { params: { id: string } }) {
   return (
     <div className="h-[80dvh] text-center flex flex-col items-center justify-center gap-10 px-8">
       <div className="flex flex-col gap-2 absolute top-24 left-1/2 -translate-x-1/2">
         <h1 className="text-xl md:text-2xl font-bold">
-          Crear nueva película en {id} hola
+          Crear nueva película en {params.id}
         </h1>
       </div>
       {/* Aquí iría el formulario de creación de película */}
