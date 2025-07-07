de88a1e0843c279c86d72dae7bf79c7516531567
Victor Manuel Martinez Cruz <vicmarc30@gmail.com>
2025-07-06 23:36:29 -0600

    feat: add form component for creating new film records with lot details

--- src/app/dashboard/new/components/form.tsx
+++ src/app/dashboard/new/components/form.tsx
@@ -1,120 +1,102 @@
-"use client";
+"use client"
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { CalendarIcon } from "lucide-react";
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { CalendarIcon } from "lucide-react"
 
-import { format } from "date-fns";
+import { format } from "date-fns"
 
-import { createClient } from "@/app/utils/supabase/client";
-import { Separator } from "@/components/ui/separator";
-import { cn } from "@/lib/utils";
-import { es } from "date-fns/locale";
-import { redirect } from "next/navigation";
-import { toast } from "sonner";
+import { createClient } from "@/app/utils/supabase/client"
+import { Separator } from "@/components/ui/separator"
+import { cn } from "@/lib/utils"
+import { es } from "date-fns/locale"
+import { redirect } from "next/navigation"
+import { toast } from "sonner"
 
 const formSchema = z.object({
   medida: z
     .number({
-      message: "Medida debe ser un número ",
+      message: "Medida debe ser un número "
     })
     .min(0, {
-      message: "Medida debe ser mayor o igual a 0",
+      message: "Medida debe ser mayor o igual a 0"
     }),
   calibre: z
     .number({
-      message: "Calilbre debe ser un número ",
+      message: "Calilbre debe ser un número "
     })
     .min(0, {
-      message: "Calilbre debe ser mayor o igual a 0",
+      message: "Calilbre debe ser mayor o igual a 0"
     }),
   caracteristicas: z.string({
-    message: "Es necesario ingresar las características",
+    message: "Es necesario ingresar las características"
   }),
   codigo_formulacion: z.string({
-    message: "Es necesario ingresar el código de formulación",
+    message: "Es necesario ingresar el código de formulación"
   }),
   configuracion: z.string({
-    message: "Es necesario ingresar la configuración",
+    message: "Es necesario ingresar la configuración"
   }),
   fecha: z.date({
-    message: "Es necesario ingresar la fecha",
+    message: "Es necesario ingresar la fecha"
   }),
   cliente: z.string({
-    message: "Es necesario ingresar el cliente",
+    message: "Es necesario ingresar el cliente"
   }),
 
   // Para lote
   num: z.string({
-    message: "Es necesario ingresar el número de lote",
+    message: "Es necesario ingresar el número de lote"
   }),
   peso_objetivo: z
     .number({
-      message: "Peso objetivo debe ser un número",
+      message: "Peso objetivo debe ser un número"
     })
     .min(0, {
-      message: "Peso objetivo debe ser mayor o igual a 0",
+      message: "Peso objetivo debe ser mayor o igual a 0"
     }),
   peso_rollo: z
     .number({
-      message: "Peso rollo debe ser un número",
+      message: "Peso rollo debe ser un número"
     })
     .min(0, {
-      message: "Peso rollo debe ser mayor o igual a 0",
-    }),
-});
+      message: "Peso rollo debe ser mayor o igual a 0"
+    })
+})
 
 export default function NewForm() {
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {},
-  });
+    defaultValues: {}
+  })
 
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof formSchema>) {
     // TODO: Crear logica a base de datos
-    const supabase = await createClient();
+    const supabase = await createClient()
 
     const { data: lote, error } = await supabase
       .from("lote")
       .insert({
         num: values.num,
         peso_objetivo: values.peso_objetivo,
-        peso_rollo: values.peso_rollo,
+        peso_rollo: values.peso_rollo
       })
-      .select();
+      .select()
 
     if (error) {
-      console.error("Error inserting lote:", error);
-      toast.error("Error al insertar el lote");
-      return;
+      console.error("Error inserting lote:", error)
+      toast.error("Error al insertar el lote")
+      return
     }
 
     const { data: pelicula, error: errorPelicula } = await supabase
@@ -127,18 +109,18 @@ export default function NewForm() {
         caracteristicas: values.caracteristicas,
         codigo_formulacion: values.codigo_formulacion,
         configuracion: values.configuracion,
-        lote: lote[0].id,
+        lote: lote[0].id
       })
-      .select();
+      .select()
 
     if (errorPelicula) {
-      console.error("Error inserting pelicula:", errorPelicula);
-      toast.error("Error al insertar la película");
-      return;
+      console.error("Error inserting pelicula:", errorPelicula)
+      toast.error("Error al insertar la película")
+      return
     }
 
-    toast.success("Película insertada correctamente");
-    redirect(`/dashboard/peliculas/${pelicula[0].id}`);
+    toast.success("Película insertada correctamente")
+    redirect(`/dashboard/peliculas/${pelicula[0].id}`)
   }
 
   return (
@@ -158,14 +140,11 @@ export default function NewForm() {
                   <FormControl>
                     <Button
                       variant={"outline"}
-                      className={cn(
-                        "w-[240px] pl-3 text-left font-normal",
-                        !field.value && "text-muted-foreground"
-                      )}
+                      className={cn("w-[240px] pl-3 text-left font-normal", !field.value && "text-muted-foreground")}
                     >
                       {field.value ? (
                         format(field.value, "PPP", {
-                          locale: es,
+                          locale: es
                         })
                       ) : (
                         <span>Escoja una fecha</span>
@@ -180,9 +159,7 @@ export default function NewForm() {
                     mode="single"
                     selected={field.value}
                     onSelect={field.onChange}
-                    disabled={(date) =>
-                      date < new Date(Date.now() - 1000 * 60 * 60 * 24 * 7)
-                    }
+                    disabled={date => date < new Date(Date.now() - 1000 * 60 * 60 * 24 * 7)}
                     captionLayout="dropdown"
                   />
                 </PopoverContent>
@@ -200,12 +177,7 @@ export default function NewForm() {
             <FormItem>
               <FormLabel>Cliente</FormLabel>
               <FormControl>
-                <Input
-                  type="text"
-                  placeholder="Cliente"
-                  {...field}
-                  onChange={(e) => field.onChange(e.target.value)}
-                />
+                <Input type="text" placeholder="Cliente" {...field} onChange={e => field.onChange(e.target.value)} />
               </FormControl>
               <FormDescription>Cliente de la película</FormDescription>
               <FormMessage />
@@ -224,7 +196,7 @@ export default function NewForm() {
                   type="number"
                   placeholder="Medida"
                   {...field}
-                  onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                  onChange={e => field.onChange(e.target.valueAsNumber)}
                 />
               </FormControl>
               <FormDescription>Medida de la película</FormDescription>
@@ -244,12 +216,10 @@ export default function NewForm() {
                   type="number"
                   placeholder="Calibre"
                   {...field}
-                  onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                  onChange={e => field.onChange(e.target.valueAsNumber)}
                 />
               </FormControl>
-              <FormDescription>
-                Este es la velocidad de la motor A.
-              </FormDescription>
+              <FormDescription>Este es la velocidad de la motor A.</FormDescription>
               <FormMessage />
             </FormItem>
           )}
@@ -268,12 +238,8 @@ export default function NewForm() {
                 </FormControl>
                 <SelectContent>
                   <SelectItem value="Punteado">Punteado</SelectItem>
-                  <SelectItem value="Abierto por un lado">
-                    Abierto por un lado
-                  </SelectItem>
-                  <SelectItem value="Abierto por los dos lados">
-                    Abierto por los dos lados
-                  </SelectItem>
+                  <SelectItem value="Abierto por un lado">Abierto por un lado</SelectItem>
+                  <SelectItem value="Abierto por los dos lados">Abierto por los dos lados</SelectItem>
                   <SelectItem value="Tratado">Tratado</SelectItem>
                 </SelectContent>
               </Select>
@@ -292,12 +258,10 @@ export default function NewForm() {
                   type="text"
                   placeholder="Codigo de Formulación"
                   {...field}
-                  onChange={(e) => field.onChange(e.target.value)}
+                  onChange={e => field.onChange(e.target.value)}
                 />
               </FormControl>
-              <FormDescription>
-                Escribe el codigo de formulacion
-              </FormDescription>
+              <FormDescription>Escribe el codigo de formulacion</FormDescription>
               <FormMessage />
             </FormItem>
           )}
@@ -306,11 +270,11 @@ export default function NewForm() {
           control={form.control}
           name="configuracion"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="col-span-1 md:col-span-3 w-full flex flex-col items-center justify-center">
               <FormLabel>Configuración de Rollos por flecha</FormLabel>
               <Select onValueChange={field.onChange} defaultValue={field.value}>
                 <FormControl>
-                  <SelectTrigger>
+                  <SelectTrigger className="w-1/2">
                     <SelectValue placeholder="Selecciona una configuración" />
                   </SelectTrigger>
                 </FormControl>
@@ -327,7 +291,7 @@ export default function NewForm() {
           )}
         />
 
-        <Separator className="md:hidden" />
+        <Separator className="w-full col-span-3" />
 
         <FormField
           control={form.control}
@@ -340,7 +304,7 @@ export default function NewForm() {
                   type="text"
                   placeholder="Número de Lote"
                   {...field}
-                  onChange={(e) => field.onChange(e.target.value)}
+                  onChange={e => field.onChange(e.target.value)}
                 />
               </FormControl>
               <FormDescription>Escribe el número de lote</FormDescription>
@@ -359,7 +323,7 @@ export default function NewForm() {
                   type="number"
                   placeholder="Peso Objetivo"
                   {...field}
-                  onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                  onChange={e => field.onChange(e.target.valueAsNumber)}
                 />
               </FormControl>
               <FormDescription>Escribe el número de lote</FormDescription>
@@ -378,7 +342,7 @@ export default function NewForm() {
                   type="number"
                   placeholder="Peso Rollo"
                   {...field}
-                  onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                  onChange={e => field.onChange(e.target.valueAsNumber)}
                 />
               </FormControl>
               <FormDescription>Escribe el peso del rollo</FormDescription>
@@ -387,10 +351,10 @@ export default function NewForm() {
           )}
         />
 
-        <Button type="submit" className="md:col-span-2 lg:col-span-3">
+        <Button type="submit" className="md:col-span-2 lg:col-span-3 cursor-pointer">
           Submit
         </Button>
       </form>
     </Form>
-  );
+  )
 }
