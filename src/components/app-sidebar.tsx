96dd66f055f2bb00afbdccb72c84d888224e13a3
Victor Manuel Martinez Cruz <vicmarc30@gmail.com>
2025-10-20 17:17:35 -0600

    feat: update sidebar navigation links and remove unused components

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -16,9 +16,7 @@ import {
 } from '@tabler/icons-react'
 import * as React from 'react'
 
-import { NavDocuments } from '@/components/nav-documents'
 import { NavMain } from '@/components/nav-main'
-import { NavSecondary } from '@/components/nav-secondary'
 import { NavUser } from '@/components/nav-user'
 import {
   Sidebar,
@@ -152,7 +150,7 @@ export function AppSidebar({ role, name, email, ...props }: AppSidebarProps) {
               asChild
               className="data-[slot=sidebar-menu-button]:!p-1.5"
             >
-              <a href="#">
+              <a href="/dashboard">
                 <IconInnerShadowTop className="!size-5" />
                 <span className="text-base font-semibold">Gessa Salaverry</span>
               </a>
@@ -162,8 +160,8 @@ export function AppSidebar({ role, name, email, ...props }: AppSidebarProps) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavDocuments items={data.documents} />
-        <NavSecondary items={data.navSecondary} className="mt-auto" />
+        {/* <NavDocuments items={data.documents} /> */}
+        {/* <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={user} />
