fbe02d1328c73fdcd256c91e3450c307b236b545
Victor Manuel Martinez Cruz <vicmarc30@gmail.com>
2025-10-20 18:21:38 -0600

    refactor: simplify middleware function and update matcher configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,11 @@
-import { updateSession } from '@/utils/supabase/middleware'
-import { type NextRequest } from 'next/server'
+// src/middleware.ts
+import { NextResponse, type NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+export function middleware(_req: NextRequest) {
+  return NextResponse.next()
 }
 
+// evita que intercepte assets y api
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
